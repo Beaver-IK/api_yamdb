02adc2a72ba6fb558ec7351f55f1037926b52598
MaratShamsutdinov <shamsutdinovu@hotmail.com>
2025-01-08 23:43:08 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'reviews.apps.ReviewsConfig',
+    'review.apps.ReviewConfig',
 ]
 
 MIDDLEWARE = [
