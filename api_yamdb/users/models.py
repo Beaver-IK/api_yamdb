b03385252e66225e1c8e1006835bffeb57b142a7
Ivan Kontsov <ivan.kontsov@gmail.com>
2025-01-10 04:25:43 +0300

    Обновлена модель Users. Обновлен менеджер модели Users. Применены миграции.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 MAX_LENGTH = 150
 MESSAGE = (f'Возможно испошльзование букв, цифр и спецсимволов @,.,+,-,_')
@@ -9,34 +11,46 @@ EMAIL_LENGTH = 254
 
 
 class CustomUserManager(BaseUserManager):
+    """кастомный менеджер пользователей."""
+
     def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_active', True)
         if not username:
-            raise ValueError('Users must have a username')
+            raise ValueError('Пользователь должен иметь username')
+        if not email:
+            raise ValueError('Пользователь должен иметь email')
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+    
+    def create_moderator(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'moderator')
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('role') != 'moderator':
+            raise ValueError('Superuser must have role=moderator.')
         return self.create_user(username, email, password, **extra_fields)
 
-class CastomUser(AbstractBaseUser):
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'admin')
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('role') != 'admin':
+            raise ValueError('Superuser must have role=admin.')
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     """Кастомная модель пользователя."""
-    
+
     ROLE_CHOICES = [
       ('user', 'User'),
       ('moderator', 'Moderator'),
       ('admin', 'Admin')
     ]
-    
+
     user_id = models.BigAutoField(primary_key=True, editable=False)
     username = models.CharField(
         max_length=MAX_LENGTH,
@@ -59,14 +73,13 @@ class CastomUser(AbstractBaseUser):
         choices=ROLE_CHOICES,
         default='user'
     )
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     objects = CustomUserManager()
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
-
 
     def __str__(self):
         return self.username
@@ -74,6 +87,17 @@ class CastomUser(AbstractBaseUser):
     def clean(self):
          super().clean()
          if not self.username:
-            raise ValidationError({'username': 'Username is required.'})
+            raise ValidationError({'username': 'Отсутствует username'})
          if not self.email:
-            raise ValidationError({'email': 'Email is required.'})
+            raise ValidationError({'email': 'Отсутствует email'})
+    
+    def generate_code(self):
+           import uuid
+           self.activation_code = str(uuid.uuid4())
+           self.date_registration_code_created = timezone.now()
+
+    def clear_code(self):
+          self.activation_code = None
+          self.date_registration_code_created = None
+    
+
